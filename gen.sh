f56e2dfdf990fa2f8ad552fd6bec874aa5b048de
wzhex <wzhexu@gmail.com>
2023-12-06 09:47:32 -0500

    wuzhe has updated data generation code

--- gen.sh
+++ gen.sh
@@ -3,8 +3,15 @@
 #SBATCH --mem=20000  # Requested Memory
 #SBATCH -t 4:00:00  # Job time limit
 
+cd /work/pi_wuzhexu_umass_edu/CPP
 
+module load python/3.8.5
+module load anaconda/2022.10
+module load gcc/5.5.0
+module load fftw/3.3.8
+
+conda activate tg3
 
  
 
-python data_generation.py -p gaussian -ns 2 -nx 64 -nt 4 -dt 0.001 -alp 4 -tau 10
+python data_generation.py -p gaussian -ns 2 -nx 64 -nt 10 -dt 0.001 -alp 3.5 -tau 10
